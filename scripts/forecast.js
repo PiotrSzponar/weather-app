843f6be77c079c5525c5c45ca2e920ed70b55e7c
Piotr Szponar <18piotrek06@gmail.com>
2019-04-24 18:16:12 +0200

    Fix https error

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'Wxj3dvSP7WBjWi0cxAOeIRsUuGgFTjxP';
 
 // get weather info
 export const getWeather = async id => {
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -13,7 +13,7 @@ export const getWeather = async id => {
 
 // get city info
 export const getCity = async city => {
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
